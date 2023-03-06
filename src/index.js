748aef1e2557e82e400127f2607cd3da42ecfd62
pranay <pranay.r@darwinbox.io>
2023-03-06 11:10:50 +0530

    made changes on friends

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const app = express();
+
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors())
+
+
+const { upload } = require('./multer/multerConfig');
+
+const signupRouter = require("./routes/signupRoute");
+app.use('/signup',signupRouter);
+
+const loginRouter = require('./routes/loginRoute');
+app.use('/login',loginRouter)
+
+
+const postRouter = require('./routes/postRoute');
+app.use('/post',postRouter);
+
+const friendsRouter=require('./routes/friendsRoute')
+app.use('/friends',friendsRouter)
+
+
 
 app.listen(7777,()=>{console.log("Connected to port 7777")});
