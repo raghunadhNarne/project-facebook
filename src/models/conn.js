b900190e24bddab33670761dae4f4c98d2e00b70
RohithSangati <sangatirohith@gmail.com>
2023-03-09 12:14:28 +0530

    modified index.html

--- src/models/conn.js
+++ src/models/conn.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose")
 
 
-// const url="mongodb+srv://projectfacebook:projectfacebook@cluster0.xc3r6ve.mongodb.net/projectfacebook"
-const url="mongodb://localhost:27017/Facebook"
+const url="mongodb+srv://projectfacebook:projectfacebook@cluster0.xc3r6ve.mongodb.net/projectfacebook"
+// const url="mongodb://localhost:27017/Facebook"
 
 
 
